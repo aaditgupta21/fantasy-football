d26e523e185d7ffa84e33e02dfda76d25672c713
TianbinLiu <l196521542@gmail.com>
2022-10-22 22:05:34 -0700

    Add home page, nav

--- src/main/java/com/nighthawk/spring_portfolio/controllers/players.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/players.java
@@ -4,15 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-public class Birds {
+public class players {
 
     // CONTROLLER handles GET request for /birds, maps it to birds() method
-    @GetMapping("/birds")
-    public String birds() {
+    @GetMapping("/players")
+    public String players() {
 
         // load HTML VIEW (birds.html)
-        return "birds";
+        return "players";
 
     }
 }
-
